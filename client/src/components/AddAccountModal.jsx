3ec8616cdac5c22b1bd92090401c06d39c3c189e
Haydar Taha Tunç <tahatunc163@gmail.com>
2024-04-25 00:48:29 +0300

    Refactor formatBalance function to include decimal places and add space for every 3 digits

--- client/src/components/AddAccountModal.jsx
+++ client/src/components/AddAccountModal.jsx
@@ -1,48 +1,233 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { getDepositOptions } from "../service/DepositOption";
+import { createAccount } from "../service/Account";
 
 const AddAccountModal = ({ handleClose, accounts }) => {
-  // Variables
   const [depositOptions, setDepositOptions] = useState([]);
   const [accountType, setAccountType] = useState("");
-  const [depositOptionID, setDepositOptionID] = useState("");
+  const [selectedDepositOption, setSelectedDepositOption] = useState(null);
   const [accountName, setAccountName] = useState("");
-  const [withdrawalAccountId, setWithdrawalAccountID] = useState("");
+  const [withdrawalAccountId, setWithdrawalAccountId] = useState("");
   const [balance, setBalance] = useState(0);
   const [currency, setCurrency] = useState("");
+  const [accountCreated, setAccountCreated] = useState(false);
+  const [alertMessage, setAlertMessage] = useState(""); // State for alert message
   const accountTypes = ["Checking", "Deposit"];
 
-  // Fetch deposit options
-  React.useEffect(() => {
+  useEffect(() => {
     getDepositOptions()
-      .then((data) => {
-        setDepositOptions(data.data);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+      .then((data) => setDepositOptions(data.data))
+      .catch((error) => console.error(error));
   }, []);
 
-  // Set the currency for the Deposit Account when the Withdrawal Account is selected
-  React.useEffect(() => {
-    if (withdrawalAccountId) {
-      setCurrency(() => {
-        const account = accounts.find(
-          (account) => account.accountID === parseInt(withdrawalAccountId)
-        );
-        return account.currency;
-      });
-    }
-  }, [withdrawalAccountId]);
+  const handleSelectDepositOption = (optionId) => {
+    setSelectedDepositOption(
+      depositOptions.find((option) => option.depositOptionID === optionId)
+    );
+  };
 
-  // Close modal when clicked outside
   const handleCloseOutside = (e) => {
     if (e.target === e.currentTarget) {
       handleClose();
     }
   };
 
-  // Create Template Bootstrap Modal
+  const handleAccountNameChange = (e) => {
+    const newName = e.target.value;
+    setAccountName(newName);
+  };
+
+  const handleCreateAccount = () => {
+    if (accountType === "Checking") {
+      const account = {
+        accountName: accountName,
+        balance: balance,
+        availableBalance: balance,
+        currency: currency,
+      };
+      createAccount(account, accountType).then((response) => {
+        setAccountCreated(true);
+        if (response.data.message === "success") {
+          setAlertMessage("Account created successfully!");
+          setTimeout(() => {
+            handleClose();
+          }, 2000);
+        } else {
+          setAlertMessage(response.data.data.error);
+        }
+      });
+    } else if (accountType === "Deposit") {
+      const depositAccountData = {
+        accountName: accountName,
+        withdrawalAccountId: withdrawalAccountId,
+        balance: balance,
+        availableBalance: balance,
+        currency: currency,
+        depositOptionID: selectedDepositOption.depositOptionID,
+      };
+      createAccount(depositAccountData, accountType).then((response) => {
+        console.log(response);
+        if (response.data.message === "success") {
+          setAccountCreated(true);
+          setAlertMessage("Account created successfully!");
+          setTimeout(() => {
+            handleClose();
+          }, 2000);
+        } else {
+          setAlertMessage(response.data.data.error);
+        }
+      });
+    }
+  };
+
+  const renderDepositOptionSelect = () => (
+    <div className="mb-3">
+      <label htmlFor="depositOption" className="form-label">
+        Deposit Option
+      </label>
+      <select
+        className="form-select"
+        id="depositOption"
+        onChange={(e) => handleSelectDepositOption(e.target.value)}
+      >
+        <option value="">Select Deposit Option</option>
+        {depositOptions.map((option) => (
+          <option key={option.depositOptionID} value={option.depositOptionID}>
+            {option.depositOptionName}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+
+  const renderDepositOptionInfo = () => {
+    if (!selectedDepositOption) return null;
+    const { depositOptionName, depositOptionDescription, interestRate, term } =
+      selectedDepositOption;
+    return (
+      <div className="card border-primary mb-3">
+        <div className="card-header bg-primary text-white">
+          <h5 className="card-title mb-0">{depositOptionName}</h5>
+        </div>
+        <div className="card-body">
+          <p className="card-text">{depositOptionDescription}</p>
+          <div className="row">
+            <div className="col">
+              <p className="card-text">
+                <strong>Interest Rate:</strong> {interestRate}
+              </p>
+            </div>
+            <div className="col">
+              <p className="card-text">
+                <strong>Term:</strong> {term}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  const renderWithdrawalAccountSelect = () => (
+    <div className="mb-3">
+      <label htmlFor="withdrawalAccountId" className="form-label">
+        Withdrawal Account
+      </label>
+      <select
+        className="form-select"
+        id="withdrawalAccountId"
+        onChange={(e) => {
+          setWithdrawalAccountId(e.target.value);
+          const selectedAccount = accounts.find(
+            (account) => account.accountID.toString() === e.target.value
+          );
+          setCurrency(selectedAccount.currency);
+        }}
+      >
+        <option value="">Select Withdrawal Account</option>
+        {accounts.map((account) => (
+          <option key={account.accountID} value={account.accountID}>
+            {account.accountName}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+
+  const renderAccountTypeSpecificFields = () => {
+    if (accountType === "Deposit") {
+      return (
+        <>
+          {renderDepositOptionSelect()}
+          {renderDepositOptionInfo()}
+          {selectedDepositOption && (
+            <>
+              <div className="mb-3">
+                <label htmlFor="accountName" className="form-label">
+                  Account Name
+                </label>
+                <input
+                  autoComplete="off"
+                  type="text"
+                  className="form-control"
+                  id="accountName"
+                  onChange={handleAccountNameChange}
+                />
+              </div>
+              {renderWithdrawalAccountSelect()}
+              <div className="mb-3">
+                <label htmlFor="balance" className="form-label">
+                  Balance
+                </label>
+                <input
+                  type="number"
+                  className="form-control"
+                  id="balance"
+                  onChange={(e) => setBalance(e.target.value)}
+                />
+              </div>
+            </>
+          )}
+        </>
+      );
+    } else if (accountType === "Checking") {
+      return (
+        <>
+          <div className="mb-3">
+            <label htmlFor="accountName" className="form-label">
+              Account Name
+            </label>
+            <input
+              autoComplete="off"
+              type="text"
+              className="form-control"
+              id="accountName"
+              onChange={(e) => setAccountName(e.target.value)}
+            />
+          </div>
+          {accountName && (
+            <div className="mb-3">
+              <label htmlFor="currency" className="form-label">
+                Currency
+              </label>
+              <select
+                className="form-select"
+                id="currency"
+                onChange={(e) => setCurrency(e.target.value)}
+              >
+                <option value="">Select Currency</option>
+                <option value="USD">USD</option>
+                <option value="EUR">EUR</option>
+                <option value="TRY">TRY</option>
+              </select>
+            </div>
+          )}
+        </>
+      );
+    }
+    return null;
+  };
+
   return (
     <div
       className="modal show"
@@ -67,6 +252,18 @@ const AddAccountModal = ({ handleClose, accounts }) => {
             ></button>
           </div>
           <div className="modal-body">
+            {alertMessage && (
+              <div
+                className={`alert ${
+                  alertMessage.includes("success")
+                    ? "alert-success"
+                    : "alert-danger"
+                }`}
+                role="alert"
+              >
+                {alertMessage}
+              </div>
+            )}
             <div className="mb-3">
               <label htmlFor="accountType" className="form-label">
                 Account Type
@@ -74,7 +271,15 @@ const AddAccountModal = ({ handleClose, accounts }) => {
               <select
                 className="form-select"
                 id="accountType"
-                onChange={(e) => setAccountType(e.target.value)}
+                onChange={(e) => {
+                  setAccountType(e.target.value);
+                  setAccountName("");
+                  setCurrency("");
+                  setBalance(0);
+                  setWithdrawalAccountId("");
+                  setSelectedDepositOption(null);
+                  setAlertMessage(""); // Clear previous alert message
+                }}
               >
                 <option value="">Select Account Type</option>
                 {accountTypes.map((type) => (
@@ -84,177 +289,19 @@ const AddAccountModal = ({ handleClose, accounts }) => {
                 ))}
               </select>
             </div>
-            {/*  When Type is Deposit */}
-            {accountType === "Deposit" && (
-              <div>
-                <div className="mb-3">
-                  <label htmlFor="depositOption" className="form-label">
-                    Deposit Option
-                  </label>
-                  <select
-                    className="form-select"
-                    id="depositOption"
-                    onChange={(e) => setDepositOptionID(e.target.value)} // Changed this line
-                  >
-                    <option value="">Select Deposit Option</option>
-                    {depositOptions.map((depositOption) => (
-                      <option
-                        key={depositOption.depositOptionID}
-                        value={depositOption.depositOptionID}
-                      >
-                        {depositOption.depositOptionName}
-                      </option>
-                    ))}
-                  </select>
-                </div>
-                <div className="mb-3">
-                  {/* Show Selected Deposit Option Name, Description, Interest Rate and Term with custom bootstrap beautiful card */}
-                  {depositOptionID && (
-                    <div className="card border-primary mb-3">
-                      <div className="card-header bg-primary text-white">
-                        <h5 className="card-title mb-0">
-                          {
-                            depositOptions.find(
-                              (option) =>
-                                option.depositOptionID === depositOptionID
-                            ).depositOptionName
-                          }
-                        </h5>
-                      </div>
-                      <div className="card-body">
-                        <p className="card-text">
-                          {
-                            depositOptions.find(
-                              (option) =>
-                                option.depositOptionID === depositOptionID
-                            ).depositOptionDescription
-                          }
-                        </p>
-                        <div className="row">
-                          <div className="col">
-                            <p className="card-text">
-                              <strong>Interest Rate:</strong>{" "}
-                              {
-                                depositOptions.find(
-                                  (option) =>
-                                    option.depositOptionID === depositOptionID
-                                ).interestRate
-                              }
-                            </p>
-                          </div>
-                          <div className="col">
-                            <p className="card-text">
-                              <strong>Term:</strong>{" "}
-                              {
-                                depositOptions.find(
-                                  (option) =>
-                                    option.depositOptionID === depositOptionID
-                                ).term
-                              }
-                            </p>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  )}
-                </div>
-                <div className="mb-3">
-                  {depositOptionID && (
-                    <div>
-                      <label htmlFor="accountName" className="form-label">
-                        Account Name
-                      </label>
-                      <input
-                        autoComplete="off"
-                        type="text"
-                        className="form-control"
-                        id="accountName"
-                        onChange={(e) => setAccountName(e.target.value)}
-                      />
-                    </div>
-                  )}
-                </div>
-                <div className="mb-3">
-                  {accountName && (
-                    <div>
-                      <label
-                        htmlFor="withdrawalAccountId"
-                        className="form-label"
-                      >
-                        Withdrawal Account
-                      </label>
-                      <select
-                        className="form-select"
-                        id="withdrawalAccountId"
-                        onChange={(e) => setWithdrawalAccountID(e.target.value)}
-                      >
-                        <option value="">Select Withdrawal Account</option>
-                        {accounts.map((account) => (
-                          <option
-                            key={account.accountID}
-                            value={account.accountID}
-                          >
-                            {account.accountName}
-                          </option>
-                        ))}
-                      </select>
-                    </div>
-                  )}
-                </div>
-                <div className="mb-3">
-                  {withdrawalAccountId && (
-                    <div>
-                      <label htmlFor="balance" className="form-label">
-                        Balance
-                      </label>
-                      <input
-                        type="number"
-                        className="form-control"
-                        id="balance"
-                        onChange={(e) => setBalance(e.target.value)}
-                      />
-                    </div>
-                  )}
-                </div>
+            {renderAccountTypeSpecificFields()}
+            {currency && (
+              <div className="text-end">
+                <button
+                  type="button"
+                  className="btn btn-primary"
+                  style={{ width: "100%" }}
+                  onClick={handleCreateAccount}
+                >
+                  <i className="bi bi-bank"></i> Create Account
+                </button>
               </div>
             )}
-            {/*  When Type is Checking */}
-            {accountType === "Checking" && (
-              <div>
-                <div className="mb-3">
-                  <label htmlFor="accountName" className="form-label">
-                    Account Name
-                  </label>
-                  <input
-                    autoComplete="off"
-                    type="text"
-                    className="form-control"
-                    id="accountName"
-                    onChange={(e) => setAccountName(e.target.value)}
-                  />
-                </div>
-                <div className="mb-3">
-                  {accountName && (
-                    <div>
-                      <label htmlFor="currency" className="form-label">
-                        Currency
-                      </label>
-                      <select
-                        className="form-select"
-                        id="currency"
-                        onChange={(e) => setCurrency(e.target.value)}
-                      >
-                        <option value="">Select Currency</option>
-                        <option value="USD">USD</option>
-                        <option value="EUR">EUR</option>
-                        <option value="TRY">TRY</option>
-                      </select>
-                    </div>
-                  )}
-                </div>
-              </div>
-            )}
-            <div className="mb-3">{currency && <div>sadasd</div>}</div>
           </div>
         </div>
       </div>
