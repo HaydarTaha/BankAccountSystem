1a3e0d9d5ef3c4df86bc4796f47c184a16be1781
Haydar Taha Tunç <tahatunc163@gmail.com>
2024-05-02 15:20:11 +0300

    Update main.jsx to import Bootstrap CSS, JS, and Icons

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+// Importing Bootstrap CSS
 import "bootstrap/dist/css/bootstrap.min.css";
+// Importing Bootstrap JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
+// Importing Bootstrap Icons
 import "bootstrap-icons/font/bootstrap-icons.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
