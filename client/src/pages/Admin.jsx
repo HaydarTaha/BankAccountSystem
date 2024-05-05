2c55917c6cedcd71350ab36225a7cd036e567a2c
Haydar Taha Tunç <tahatunc163@gmail.com>
2024-05-05 22:40:55 +0300

    refactor: Update Admin.jsx to fetch and display data for selected page

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -1,6 +1,13 @@
 import React from "react";
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import AdminPanelSidebar from "../components/Admin/AdminPanelSidebar";
+import {
+  createData,
+  getAllData,
+  updateData,
+  deleteData,
+} from "../service/Crud";
+import Table from "../components/Admin/Table";
 
 const Admin = () => {
   const { page } = useParams();
@@ -11,18 +18,39 @@ const Admin = () => {
     { name: "Deposit Options", path: "deposit-options" },
   ];
 
+  const [data, setData] = React.useState([]);
+  const [isFetching, setIsFetching] = React.useState(true);
+
+  React.useEffect(() => {
+    setIsFetching(true);
+    getAllData(page)
+      .then((response) => {
+        setData(response.data);
+        setIsFetching(false);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [page]);
+
   return (
     <div className="mb-3">
       <h1>Admin Panel - {pages.find((p) => p.path === page).name}</h1>
       <div className="row">
         <AdminPanelSidebar pages={pages} />
-        <div className="col-md-9 text-center p-5">
+        <div className="col-md-9 text-center">
           {pages.map((p) => {
             if (p.path === page) {
               return (
                 <div key={p.path}>
-                  <h2>{p.name}</h2>
-                  <p>Content for {p.name}</p>
+                  <h1>{p.name}</h1>
+                  <Table
+                    data={data}
+                    tableName={p.name.slice(0, -1)}
+                    isFetching={isFetching}
+                    setData={setData}
+                    setIsFetching={setIsFetching}
+                  />
                 </div>
               );
             }
